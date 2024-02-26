1d2fca1bae6ab2a02542ed24f54fb2e0f75c411e
Jai Dev <jaidev@192.168.1.5>
2024-02-27 00:18:34 +0530

    conditional layout mapping using usePathname hook

--- src/app/login/layout.js
+++ src/app/login/layout.js
@@ -1,14 +1,24 @@
+"use client"
 import Link from "next/link";
 import '@/app/login/page.css'
-export default function Layout({children}){
+import { usePathname } from "next/navigation";
+export default function Layout({ children }) {
+    const pathName = usePathname();
+    console.log(pathName);
     return (
         <div>
-            <ul className="login-menu">
-                <li> <h4>Login Navbar </h4></li>
-                <li> <Link href="/login/admin"> Admin Login </Link></li>
-                <li> <Link href="/login/user"> User Login </Link></li>
-                <li> <Link href="/login"> Main Login </Link></li>
-            </ul>
+            {
+                pathName === '/login/admin'
+                    ?
+                    <ul className="login-menu">
+                        <li> <h4>Login Navbar </h4></li>
+                        <li> <Link href="/login/admin"> Admin Login </Link></li>
+                        <li> <Link href="/login/user"> User Login </Link></li>
+                        <li> <Link href="/login"> Main Login </Link></li>
+                    </ul>
+                    : <Link href="/">Home</Link>
+            }
+
             {
                 children
             }
